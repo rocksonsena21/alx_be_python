59f482d364e54a9141c8d7c7052b325c57853463
Rockson <rocksonsena21@gmail.com>
2025-11-06 17:47:09 +0000

    added the operations symbols

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,11 @@
 number1 = 10
 number2 = 5
 
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+
 print("Addition of 10 and 5 is 15")
 print("Subtraction of 10 and 5 is 5")
 print("Multiplication of 10 and 5 is 50")
