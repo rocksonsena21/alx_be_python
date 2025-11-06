749aa342c28969edc824ca9f80c2b88ac3d0c036
Rockson <rocksonsena21@gmail.com>
2025-11-06 18:00:54 +0000

    rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+
+
+# Define the dimensions of the rectangle
+length = 10
+width = 5
+
+# Calculate the area
+area = length * width
+
+# Display the result
+print("The area of the rectangle is:", area)
